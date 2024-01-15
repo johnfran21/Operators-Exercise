7fda29865b5836569a5754d7604dc80145de9fd1
John Franzese <johnfranzese21@gmail.com>
2024-01-15 12:00:48 -0600

    Operator Exercise Updates

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int sum = a + b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} equals {sum} and {a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine("Give number for chosen Radius");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+        }
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
         }
     }
 }
